7ba3f0c51a47117cefc812d6aeac0e1a47997523
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-08-07 20:25:43 +0300

    nastiakomarenko: added Google test and HomePage to verify search

--- src/test/java/nastiakomarenko/GoogleTest.java
+++ src/test/java/nastiakomarenko/GoogleTest.java
@@ -7,26 +7,22 @@ import org.openqa.selenium.By;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.naming.directory.SearchResult;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.testng.AssertJUnit.assertTrue;
 
-public class GoogleTest {
-    private final String FUNNY_DOGS = "funny dogs";
+public class GoogleTest extends HomePage{
 
-    @BeforeMethod
-    public void setUp() {
-        open("https://www.google.com");
-        $(By.name("q")).val(FUNNY_DOGS).pressEnter();
-    }
 
     @Test
-    public void verify_search_dogs() {
+    public void verifyThatMethodSearchesDogs() {
         $x("//a/h3[@class = 'LC20lb MBeuO DKV0Md']").shouldHave(text("dogs"));
     }
 
     @Test
-    public void verify_search_ninth_link(){
+    public void verifyThatMethodSearchesTheNinthLink(){
         String ninthLink = $x("(//a)[9]").getAttribute("href");
         $x("(//a)[9]").click();
 
@@ -36,46 +32,43 @@ public class GoogleTest {
     }
 
     @Test
-    public void verify_google_page_isOpen(){
+    public void verifyThatGooglePageIsOpen(){
         $x("//div[@class='logo']").click();
         $x("//img[@class='lnXdpd']").shouldBe(visible);
     }
 
     @Test
-    public void verify_fifth_page(){
+    public void verifyThatFifthPageHasText(){
         $x("//tr[@jsname = 'TeSSVd']//a[contains(@aria-label, '5')]").click();
         $x("//a/h3[@class = 'LC20lb MBeuO DKV0Md']").shouldHave(text("dogs"));
     }
 
     @Test
-    public void verify_nine_links_displayed(){
+    public void verifyThatNineLinksAreDisplayed(){
         $$x("//h3[@class = 'LC20lb MBeuO DKV0Md']").shouldHave(CollectionCondition.sizeGreaterThanOrEqual(9));
     }
 
     @Test
-    public void verify_search_contains_kitten(){
+    public void verifyThatMethodContainsText(){
         $x("//input[@class = 'gLFyf gsfi']").clear();
         $(By.name("q")).val("funny kitten").pressEnter();
         $x("//div[@class = 'mSA5Bd']").shouldHave(text("kitten")).shouldNotHave(text("dogs"));
     }
 
     @Test
-    public void verify_google_logo_isDisplayed(){
+    public void verifyThatGoogleLogoIsDisplayed(){
         $x("//div[@class = 'logo']").shouldBe(visible);
     }
 
     @Test
-    public void verify_nextLink_isDisplayed(){
+    public void verifyThatNextLinkIsDisplayed(){
         $x("//a[@id='pnnext']/span[@class]").shouldBe(visible);
     }
 
     @Test
-    public void verify_links_areDisplayed(){
+    public void verifyThatLinksAreDisplayed(){
         $x("//a[@id='pnnext']/span[@class]").shouldBe(visible);
         $x("//tr[@jsname = 'TeSSVd']//a[contains(@aria-label, '4')]").click();
         $x("//span[@class='SJajHc NVbCr']").shouldBe(visible);
     }
-
 }
-
-
