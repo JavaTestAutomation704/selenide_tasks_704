cb0775b6a5a2e47e1a46c096f3edb7afbbc51cf6
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-08-16 23:15:19 +0300

    ozakharchuk: edited Google test to verify search

--- src/test/java/ozakharchuk/GoogleTestRunner.java
+++ src/test/java/ozakharchuk/GoogleTestRunner.java
@@ -1,11 +1,22 @@
 package ozakharchuk;
 
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class GoogleTestRunner {
 
     protected GooglePage homePage;
     protected SearchResultPage searchResultsPage;
+
+    @BeforeClass
+    public void setUpBrowser() {
+        Configuration.browser = "chrome";
+        Configuration.browserCapabilities = new ChromeOptions()
+                .addArguments("--lang=en-GB");
+        Configuration.timeout = 10;
+    }
 
     @BeforeMethod
     public void openHomePageAndSearch() {
