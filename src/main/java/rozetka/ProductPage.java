60a7cf78e42e6883c37994805f1201093859bdb1
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-17 19:23:39 +0300

    ykireyeva: added buyOnCredit() method to ProductPage

--- src/main/java/rozetka/ProductPage.java
+++ src/main/java/rozetka/ProductPage.java
@@ -1,4 +1,11 @@
 package rozetka;
 
+import static com.codeborne.selenide.Selenide.$x;
+
 public class ProductPage extends HeaderComponent {
+
+    public ProductPage buyOnCredit() {
+        $x("//app-product-credit").click();
+        return this;
+    }
 }
