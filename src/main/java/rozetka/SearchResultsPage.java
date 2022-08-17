aeb818c29c87428ac8a6f91a1529c4924733a963
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-08-17 15:30:59 +0300

    mvynokur: added methods: ShoppingCartModal: - getCartReceipt() SearchResultsPage: - addProductViaShoppingCartIcon() - getProductStatuses() - getProductTitles() - getProductPrice() ResultsFilterComponent: - toggleFilter() HomePage: - open() HeaderComponent: - search() - openShoppingCartViaHeader() CategoryPage: selectCategory()

--- src/main/java/rozetka/SearchResultsPage.java
+++ src/main/java/rozetka/SearchResultsPage.java
@@ -1,4 +1,23 @@
 package rozetka;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchResultsPage extends ResultsFilterComponent {
+
+    public SearchResultsPage addProductViaShoppingCartIcon(int number) {
+        return this;
+    }
+
+    public List<String> getProductStatuses() {
+        return new ArrayList<>();
+    }
+
+    public List<String> getProductTitles() {
+        return new ArrayList<>();
+    }
+
+    public int getProductPrice(int number) {
+        return 0;
+    }
 }
