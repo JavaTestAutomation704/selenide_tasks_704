5a85db0a5d7648d38fde59a6856643ceb10b6d36
Jaroslav Tarasovych <staryk90@gmail.com>
2022-08-17 15:54:01 +0300

    ytarasovych: added isHeaderVisible(), isTotalModalVisible(), isContactsModalVisible() and isOrderModalVisible()  in CheckoutPage to verify

--- src/main/java/rozetka/CheckoutPage.java
+++ src/main/java/rozetka/CheckoutPage.java
@@ -1,4 +1,20 @@
 package rozetka;
 
 public class CheckoutPage extends HeaderComponent {
+
+    public boolean isHeaderVisible() {
+        return true;
+    }
+
+    public boolean isTotalModalVisible() {
+        return true;
+    }
+
+    public boolean isContactsModalVisible() {
+        return true;
+    }
+
+    public boolean isOrderModalVisible() {
+        return true;
+    }
 }
