12e29690ea36a9e2071f5d89e9bf47809cc9933d
DankoMaksym <dankomaksym@gmail.com>
2022-08-18 00:26:12 +0300

    dankomax: add getTitle to ProductCharacteristicsPage

--- src/main/java/rozetka/ProductCharacteristicsPage.java
+++ src/main/java/rozetka/ProductCharacteristicsPage.java
@@ -1,4 +1,10 @@
 package rozetka;
 
+import static com.codeborne.selenide.Selenide.$x;
+
 public class ProductCharacteristicsPage extends HeaderComponent {
+
+    public String getTitle() {
+        return $x("//h1[@class='product__title']").text();
+    }
 }
