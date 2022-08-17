24fcc017adb864d4d4006c68fb42f62abb317f1b
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-17 19:21:54 +0300

    ykireyeva: added getResultsAmount() method to SearchResultsPage

--- src/main/java/rozetka/SearchResultsPage.java
+++ src/main/java/rozetka/SearchResultsPage.java
@@ -5,8 +5,15 @@ import static com.codeborne.selenide.Selenide.$x;
 public class SearchResultsPage extends ResultsFilterComponent {
     String productXpath = "(//div[contains(@class, 'goods-tile ')])[%d]";
 
-    public ProductPage open(int productNumber){
+    public ProductPage open(int productNumber) {
         $x(String.format(productXpath, productNumber)).click();
         return new ProductPage();
+    }
+
+    public int getResultsAmount() {
+        return Integer.parseInt(
+                $x("//p[contains(@class, 'selection')]")
+                        .getText()
+                        .replaceAll("[^0-9]", ""));
     }
 }
