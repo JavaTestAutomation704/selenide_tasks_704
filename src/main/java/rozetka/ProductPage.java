dd2a1992f4c495788af3f979e84cd8c06708d964
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-08-17 23:58:46 +0300

    added new methods into CatalogModal, ProductPage and HeaderComponent classes

--- src/main/java/rozetka/ProductPage.java
+++ src/main/java/rozetka/ProductPage.java
@@ -1,9 +1,15 @@
 package rozetka;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import static com.codeborne.selenide.Selenide.$x;
 import static utils.WebElementUtil.isVisible;
 
 public class ProductPage extends HeaderComponent {
+
+    @FindBy(xpath = "//div[@class='product-prices__inner ng-star-inserted']")
+    private WebElement price;
 
     public ProductPage buyOnCredit() {
         $x("//app-product-credit").click();
