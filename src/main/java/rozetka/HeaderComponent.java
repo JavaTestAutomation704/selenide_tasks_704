12a266a8ef46a6488dd2cde13b314d91582a02b3
Jaroslav Tarasovych <staryk90@gmail.com>
2022-08-17 15:51:28 +0300

    ytarasovych: added isAuthorizationButtonVisible(), isRegistrationButtonVisible(), isLocationSelectionVisible(), isHelpCenterButtonVisible() and isContactUsButtonVisible() in HeaderComponent to verify

--- src/main/java/rozetka/HeaderComponent.java
+++ src/main/java/rozetka/HeaderComponent.java
@@ -1,4 +1,24 @@
 package rozetka;
 
 public class HeaderComponent {
+
+    public boolean isAuthorizationButtonVisible() {
+        return true;
+    }
+
+    public boolean isRegistrationButtonVisible() {
+        return true;
+    }
+
+    public boolean isLocationSelectionVisible() {
+        return true;
+    }
+
+    public boolean isHelpCenterButtonVisible() {
+        return true;
+    }
+
+    public boolean isContactUsButtonVisible() {
+        return true;
+    }
 }
