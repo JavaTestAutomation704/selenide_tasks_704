69f8cfd2355b54864d4181de1b4e69a92284092c
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-17 19:20:32 +0300

    ykireyeva: added productXpath & open() method to SearchResultsPage

--- src/main/java/rozetka/SearchResultsPage.java
+++ src/main/java/rozetka/SearchResultsPage.java
@@ -1,4 +1,12 @@
 package rozetka;
 
+import static com.codeborne.selenide.Selenide.$x;
+
 public class SearchResultsPage extends ResultsFilterComponent {
+    String productXpath = "(//div[contains(@class, 'goods-tile ')])[%d]";
+
+    public ProductPage open(int productNumber){
+        $x(String.format(productXpath, productNumber)).click();
+        return new ProductPage();
+    }
 }
