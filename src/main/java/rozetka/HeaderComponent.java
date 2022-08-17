bcbec757ac11f40ec8f0a56aa5949aeb41c4b5f2
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-17 19:25:12 +0300

    ykireyeva: added logInViaUserIcon() method to HeaderComponent

--- src/main/java/rozetka/HeaderComponent.java
+++ src/main/java/rozetka/HeaderComponent.java
@@ -1,4 +1,11 @@
 package rozetka;
 
+import static com.codeborne.selenide.Selenide.$x;
+
 public class HeaderComponent {
+
+    public HeaderComponent logInViaUserIcon() {
+        $x("//rz-user").click();
+        return this;
+    }
 }
