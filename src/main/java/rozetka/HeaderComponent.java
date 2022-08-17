3faf81d8e163ced0ace850722ba857c5117e3966
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-17 19:28:27 +0300

    ykireyeva: added isLogInModalVisible() method to HeaderComponent

--- src/main/java/rozetka/HeaderComponent.java
+++ src/main/java/rozetka/HeaderComponent.java
@@ -1,11 +1,16 @@
 package rozetka;
 
 import static com.codeborne.selenide.Selenide.$x;
+import static utils.WebElementUtil.isVisible;
 
 public class HeaderComponent {
 
     public HeaderComponent logInViaUserIcon() {
         $x("//rz-user").click();
         return this;
+    }
+
+    public boolean isLogInModalVisible() {
+        return isVisible("//rz-user-identification");
     }
 }
