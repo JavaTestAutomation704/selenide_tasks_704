79884443957aab1007b08d7fe1d8dfd9c966511d
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-08-19 00:09:03 +0300

    nastiakomarenko: added isRegisterButtonVisible() and openCatalog() methods for Header

--- src/main/java/com/softserveinc/ita/rozetka/components/Header.java
+++ src/main/java/com/softserveinc/ita/rozetka/components/Header.java
@@ -1,10 +1,12 @@
 package com.softserveinc.ita.rozetka.components;
 
-import static com.codeborne.selenide.Selenide.$x;
+import com.softserveinc.ita.rozetka.modals.CatalogModal;
+
+import static utils.WebElementUtil.isVisible;
 
 public class Header {
-    public MobileMenu openMobileMenu() {
-        $x("//rz-mobile-user-menu/button").click();
-        return new MobileMenu();
+    public boolean isRegisterButtonVisible() {
+        return isVisible("//button[@class='auth-modal__register-link button button--link ng-star-inserted']");
     }
+    public CatalogModal openCatalog(){return new CatalogModal();}
 }
