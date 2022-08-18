a497a85abba9187968cc25d2e053e80f47501739
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-08-19 00:11:11 +0300

    nastiakomarenko: added field WebElement price for ProductPage

--- src/main/java/com/softserveinc/ita/rozetka/ProductPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/ProductPage.java
@@ -1,5 +1,11 @@
 package com.softserveinc.ita.rozetka;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class ProductPage extends BasePage {
-    // implement
+
+    @FindBy(xpath = "//div[@class='product-prices__inner ng-star-inserted']")
+    private WebElement price;
+
 }
