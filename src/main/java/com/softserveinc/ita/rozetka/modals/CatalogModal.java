ef0bd01680a50cf3b456c5f80948f1f2bb0cc9ef
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-08-19 00:02:45 +0300

    nastiakomarenko: added categoriesBlockIsDisplayed() and isCategoriesListActivated() methods for CatalogModal

--- src/main/java/com/softserveinc/ita/rozetka/modals/CatalogModal.java
+++ src/main/java/com/softserveinc/ita/rozetka/modals/CatalogModal.java
@@ -1,4 +1,36 @@
 package com.softserveinc.ita.rozetka.modals;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class CatalogModal {
+
+    @FindBy(xpath = "//ul[@class = 'menu-categories ng-star-inserted']")
+    private WebElement categoriesBlock;
+
+    @FindBy(xpath = "//div[@class='menu-wrapper menu-wrapper_state_animated']")
+    private List<WebElement> categoriesNameList;
+
+    public boolean categoriesBlockIsDisplayed() {
+        try {
+            return categoriesBlock.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+    public boolean isCategoriesListActivated() {
+        for (WebElement categoryElement : categoriesNameList) {
+            try {
+                categoryElement.isDisplayed();
+                categoryElement.isEnabled();
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
