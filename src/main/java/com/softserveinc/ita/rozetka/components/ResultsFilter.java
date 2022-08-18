0feac12786d9ce717ea1e1613a1611b4424ce6a6
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-08-18 10:39:32 +0300

    added enum ProductFilter added ResultsFilter implementation

--- src/main/java/com/softserveinc/ita/rozetka/components/ResultsFilter.java
+++ src/main/java/com/softserveinc/ita/rozetka/components/ResultsFilter.java
@@ -3,11 +3,19 @@ package com.softserveinc.ita.rozetka.components;
 import com.softserveinc.ita.rozetka.SearchResultsPage;
 import utils.ProductFilter;
 
+import java.util.List;
+
 import static com.codeborne.selenide.Selenide.$x;
 
 public class ResultsFilter extends Header {
-    public SearchResultsPage toggleFilter(ProductFilter... type) {
-        for (ProductFilter filter : type) {
+
+    public SearchResultsPage toggleFilter(ProductFilter type) {
+        $x(String.format("//a[@data-id = '%s']", type.getFilterXpath())).click();
+        return new SearchResultsPage();
+    }
+
+    public SearchResultsPage toggleFilter(List<ProductFilter> filters) {
+        for (ProductFilter filter : filters) {
             $x(String.format("//a[@data-id = '%s']", filter.getFilterXpath())).click();
         }
         return new SearchResultsPage();
--- src/main/java/utils/ProductFilter.java
+++ src/main/java/utils/ProductFilter.java
@@ -3,7 +3,10 @@ package utils;
 public enum ProductFilter {
 
     AVAILABLE("Є в наявності"),
-    OUT_OF_STOCK("Немає в наявності");
+    RUN_OUT("Немає в наявності"),
+    WHITE_COLOR("Білий"),
+    ROZETKA_SELLER("Rozetka"),
+    OTHER_SELLERS("Інші продавці");
 
     private final String filterXpath;
 
