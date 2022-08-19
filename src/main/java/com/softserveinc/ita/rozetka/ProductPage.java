969b9f3a50ee35f86558c0575ccca07546d4edac
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-19 15:03:12 +0300

    ykireyeva: created CreditModal & refactored startPurchaseOnCredit() method

--- src/main/java/com/softserveinc/ita/rozetka/ProductPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/ProductPage.java
@@ -1,12 +1,15 @@
 package com.softserveinc.ita.rozetka;
 
+import com.softserveinc.ita.rozetka.modals.CreditModal;
+
 import static com.codeborne.selenide.Selenide.$x;
 import static utils.WebElementUtil.isVisible;
 
 public class ProductPage extends BasePage {
-    public ProductPage buyOnCredit() {
+
+    public CreditModal startPurchaseOnCredit() {
         $x("//app-product-credit").click();
-        return this;
+        return new CreditModal();
     }
 
     public boolean isCreditModalVisible() {
---
+++ src/main/java/com/softserveinc/ita/rozetka/modals/CreditModal.java
@@ -0,0 +1,5 @@
+package com.softserveinc.ita.rozetka.modals;
+
+public class CreditModal {
+    // empty class for use
+}
