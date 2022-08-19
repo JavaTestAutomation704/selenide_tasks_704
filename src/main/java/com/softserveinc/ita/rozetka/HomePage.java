236428f378ac3be5f088abb5312462ad03deaf3e
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-08-19 08:20:46 +0300

    ozakharchuk: added method to HeaderComponent, HomePage,ShoppingCart

--- src/main/java/com/softserveinc/ita/rozetka/HomePage.java
+++ src/main/java/com/softserveinc/ita/rozetka/HomePage.java
@@ -2,9 +2,19 @@ package com.softserveinc.ita.rozetka;
 
 import com.codeborne.selenide.Selenide;
 
+import static utils.WebElementUtil.isVisible;
+
 public class HomePage extends BasePage {
     public HomePage open() {
         Selenide.open("https://rozetka.com.ua/ua/");
         return this;
+    }
+
+    public boolean isSmallCartSectionVisible(){
+        return isVisible("//rz-app-small-cart");
+    }
+
+    public boolean isMainCategoriesSectionVisible(){
+        return isVisible("//rz-app-fat-menu-tablet");
     }
 }
--- src/main/java/com/softserveinc/ita/rozetka/modals/ShoppingCartModal.java
+++ src/main/java/com/softserveinc/ita/rozetka/modals/ShoppingCartModal.java
@@ -1,4 +1,24 @@
 package com.softserveinc.ita.rozetka.modals;
 
+import static com.codeborne.selenide.Selenide.$x;
+
 public class ShoppingCartModal {
+
+    public ShoppingCartModal increaseProductQuantity(){
+        $x("//button[@data-testid='cart-counter-increment-button']").click();
+        return this;
+    }
+
+    public ShoppingCartModal decreaseProductQuantity(){
+        $x("//button[@data-testid='cart-counter-decrement-button']").click();
+        return this;
+    }
+
+    public int getPrice(int productNumber){
+        return Integer.parseInt($x("//p[@data-testid='cost']").text());
+    }
+
+    public int getProductQuantity(){
+        return Integer.parseInt($x("//input[@data-testid='cart-counter-input']").text());
+    }
 }
