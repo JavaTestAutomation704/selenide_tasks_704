778126fffe1ed1f228bd28c741d461c670597fca
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-08-19 18:55:25 +0300

    mvynokur: added Categories enum; added CategoryPage open method to HomePage; refactored ProductFilter enum using Lombok;

--- src/main/java/com/softserveinc/ita/rozetka/HomePage.java
+++ src/main/java/com/softserveinc/ita/rozetka/HomePage.java
@@ -1,10 +1,19 @@
 package com.softserveinc.ita.rozetka;
 
 import com.codeborne.selenide.Selenide;
+import com.softserveinc.ita.rozetka.data.Categories;
+
+import static com.codeborne.selenide.Selenide.$x;
 
 public class HomePage extends BasePage {
+
     public HomePage open() {
         Selenide.open("https://rozetka.com.ua/ua/");
         return this;
+    }
+
+    public CategoryPage open(Categories category) {
+        $x(String.format("//div[@class = 'fat-wrap']//a[contains(@href, '%s')]", category.getCategoryXpath())).click();
+        return new CategoryPage();
     }
 }
---
+++ src/main/java/com/softserveinc/ita/rozetka/data/Categories.java
@@ -0,0 +1,16 @@
+package com.softserveinc.ita.rozetka.data;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Categories {
+
+    NOTEBOOKS_COMPUTERS("computers-notebooks"),
+    PHONES_TV_ELECTRONICS("telefony-tv-i-ehlektronika"),
+    GAMER_PRODUCTS("game-zone");
+
+    @NonNull private final String categoryXpath;
+}
