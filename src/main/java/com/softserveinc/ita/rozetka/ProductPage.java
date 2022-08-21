5db78164dfd44df392a7996b14afba08d6869c3e
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-08-21 12:21:13 +0300

    nastiakomarenko: added methods for 5 classes

--- src/main/java/com/softserveinc/ita/rozetka/ProductPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/ProductPage.java
@@ -1,5 +1,9 @@
 package com.softserveinc.ita.rozetka;
 
+import static com.codeborne.selenide.Selenide.$;
+
 public class ProductPage extends BasePage {
-    // implement
+    public String getPrice() {
+        return $("//div[@class='product-prices__inner ng-star-inserted']").text();
+    }
 }
--- src/main/java/com/softserveinc/ita/rozetka/modals/CatalogModal.java
+++ src/main/java/com/softserveinc/ita/rozetka/modals/CatalogModal.java
@@ -1,4 +1,9 @@
 package com.softserveinc.ita.rozetka.modals;
+import static com.codeborne.selenide.Selenide.$;
 
 public class CatalogModal {
+
+    public String getCategoriesBlock() {
+        return $("//ul[@class = 'menu-categories ng-star-inserted']").text();
+    }
 }
