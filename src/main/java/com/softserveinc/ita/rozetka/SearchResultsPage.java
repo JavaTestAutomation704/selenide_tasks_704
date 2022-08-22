e5bbee6c207ce07f9213cc9751a605a790dbc591
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-08-22 22:46:00 +0300

    ozakharchuk: removed unnecessary methods

--- src/main/java/com/softserveinc/ita/rozetka/SearchResultsPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/SearchResultsPage.java
@@ -2,24 +2,8 @@ package com.softserveinc.ita.rozetka;
 
 import com.softserveinc.ita.rozetka.components.ResultsFilter;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static utils.WebElementUtil.getCollectionSize;
-import static utils.WebElementUtil.getText;
-
-
 public class SearchResultsPage extends BasePage {
     public ResultsFilter getFilter() {
         return new ResultsFilter();
-    }
-
-    public List<String> getProductStates() {
-        String productsStateXpath = "//span[contains(@class, 'promo-label_type_used')]";
-        List<String> states = new ArrayList<>();
-        for (int i = 0; i < getCollectionSize(productsStateXpath); i++) {
-            states.add(getText(String.format("([%s])[%s]", productsStateXpath, i)));
-        }
-        return states;
     }
 }
--- src/main/java/com/softserveinc/ita/rozetka/modals/ShoppingCartModal.java
+++ src/main/java/com/softserveinc/ita/rozetka/modals/ShoppingCartModal.java
@@ -1,24 +1,4 @@
 package com.softserveinc.ita.rozetka.modals;
 
-import static com.codeborne.selenide.Selenide.$x;
-
 public class ShoppingCartModal {
-
-    public ShoppingCartModal increaseProductQuantity(int productNumber) {
-        $x(String.format("//(button[@data-testid='cart-counter-increment-button'])[%s]", productNumber)).click();
-        return this;
-    }
-
-    public ShoppingCartModal decreaseProductQuantity(int productNumber) {
-        $x(String.format("//(button[@data-testid='cart-counter-decrement-button'])[%s]", productNumber)).click();
-        return this;
-    }
-
-    public int getPrice(int productNumber) {
-        return Integer.parseInt($x(String.format("//(p[@data-testid='cost'])[%s]", productNumber)).text());
-    }
-
-    public int getProductQuantity(int productNumber) {
-        return Integer.parseInt($x(String.format("//(input[@data-testid='cart-counter-input)[%s]", productNumber)).text());
-    }
 }
