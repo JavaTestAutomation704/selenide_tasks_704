017ac3a1926cd2cbbb8f7b1ea1f4c7cbb536bb03
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-22 15:01:43 +0300

    ykireyeva: added open() method to CreditModal

--- src/main/java/com/softserveinc/ita/rozetka/ProductPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/ProductPage.java
@@ -2,12 +2,9 @@ package com.softserveinc.ita.rozetka;
 
 import com.softserveinc.ita.rozetka.modals.CreditModal;
 
-import static com.codeborne.selenide.Selenide.$x;
-
 public class ProductPage extends BasePage {
 
     public CreditModal startPurchaseOnCredit() {
-        $x("//app-product-credit").click();
-        return new CreditModal();
+        return new CreditModal().open();
     }
 }
--- src/main/java/com/softserveinc/ita/rozetka/modals/CreditModal.java
+++ src/main/java/com/softserveinc/ita/rozetka/modals/CreditModal.java
@@ -1,8 +1,14 @@
 package com.softserveinc.ita.rozetka.modals;
 
+import static com.codeborne.selenide.Selenide.$x;
 import static utils.WebElementUtil.isVisible;
 
 public class CreditModal {
+
+    public CreditModal open() {
+        $x("//app-product-credit").click();
+        return this;
+    }
 
     public boolean isCreditModalVisible() {
         return isVisible("//credit-modal");
