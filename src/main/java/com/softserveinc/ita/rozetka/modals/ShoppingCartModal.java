a4cd939b427c5182bb2e4629fc7964e532cad1c8
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-08-22 07:05:52 +0300

    ozakharchuk: edited method in ShoppingCart

--- src/main/java/com/softserveinc/ita/rozetka/modals/ShoppingCartModal.java
+++ src/main/java/com/softserveinc/ita/rozetka/modals/ShoppingCartModal.java
@@ -4,21 +4,21 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class ShoppingCartModal {
 
-    public ShoppingCartModal increaseProductQuantity(){
-        $x("//button[@data-testid='cart-counter-increment-button']").click();
+    public ShoppingCartModal increaseProductQuantity(int productNumber) {
+        $x(String.format("//(button[@data-testid='cart-counter-increment-button'])[%s]", productNumber)).click();
         return this;
     }
 
-    public ShoppingCartModal decreaseProductQuantity(){
-        $x("//button[@data-testid='cart-counter-decrement-button']").click();
+    public ShoppingCartModal decreaseProductQuantity(int productNumber) {
+        $x(String.format("//(button[@data-testid='cart-counter-decrement-button'])[%s]", productNumber)).click();
         return this;
     }
 
-    public int getPrice(int productNumber){
-        return Integer.parseInt($x("//p[@data-testid='cost']").text());
+    public int getPrice(int productNumber) {
+        return Integer.parseInt($x(String.format("//(p[@data-testid='cost'])[%s]", productNumber)).text());
     }
 
-    public int getProductQuantity(){
-        return Integer.parseInt($x("//input[@data-testid='cart-counter-input']").text());
+    public int getProductQuantity(int productNumber) {
+        return Integer.parseInt($x(String.format("//(input[@data-testid='cart-counter-input)[%s]", productNumber)).text());
     }
 }
