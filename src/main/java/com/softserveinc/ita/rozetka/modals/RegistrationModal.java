15859282c2b62873d53f47058435f8832a6864e2
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-08-23 15:27:54 +0300

    nastiakomarenko: refactored isOpen() method

--- src/main/java/com/softserveinc/ita/rozetka/modals/RegistrationModal.java
+++ src/main/java/com/softserveinc/ita/rozetka/modals/RegistrationModal.java
@@ -3,7 +3,7 @@ package com.softserveinc.ita.rozetka.modals;
 import static utils.WebElementUtil.isVisible;
 
 public class RegistrationModal {
-    public boolean isRegistrationModalVisible() {
+    public boolean isOpen() {
         return isVisible("//div[@class='modal__holder modal__holder_show_animation modal__holder--medium']");
     }
 }
