903766bbdf19fb17b4ad2552da0a02fa8599a7e8
Olenka <olenkadashinich@gmail.com>
2022-08-23 13:55:36 +0300

    odashynych: change city test

--- src/test/java/com/softserveinc/ita/rozetka/MobileMenuTests/ChangeCityTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/MobileMenuTests/ChangeCityTest.java
@@ -3,21 +3,18 @@ package com.softserveinc.ita.rozetka.MobileMenuTests;
 import com.softserveinc.ita.rozetka.TestRunner;
 import com.softserveinc.ita.rozetka.components.Header;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ChangeCityTest extends TestRunner {
-    @DataProvider(name = "city")
-    public Object[][] data() {
-        return new Object[][]{
-                {"Одеса"},
-                {"Запоріжжя"}
-        };
-    }
-
-    @Test(dataProvider = "city")
-    public void changeCityTest(String city) {
-        Header header = homePage.getHeader().openMobileMenu().changeCity(city);
-        Assert.assertEquals(header.openMobileMenu().getCity(), city);
+    @Test()
+    public void changeCityTest() {
+        String expectedCity = "Одеса";
+        Header header = homePage
+                .getHeader()
+                .openMobileMenu()
+                .changeCity(expectedCity);
+        String actualCity = header.openMobileMenu().getCity();
+        Assert.assertEquals(actualCity, expectedCity);
+        // will be added one more assert after merge
     }
 }
