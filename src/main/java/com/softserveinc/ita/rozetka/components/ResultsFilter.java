0722df7f832da5e2f3eebd76d8fa55e18a2e3853
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-24 18:30:08 +0300

    ykireyeva: added scrollIntoView(false) to filter() methods

--- src/main/java/com/softserveinc/ita/rozetka/components/ResultsFilter.java
+++ src/main/java/com/softserveinc/ita/rozetka/components/ResultsFilter.java
@@ -10,12 +10,16 @@ import static com.codeborne.selenide.Selenide.$x;
 public class ResultsFilter extends Header {
 
     public SearchResultsPage filter(ProductFilter type) {
-        $x(String.format("//a[@data-id = '%s']", type.getFilterXpath())).click();
+        $x(String.format("//a[@data-id = '%s']", type.getFilterXpath()))
+                .scrollIntoView(false)
+                .click();
         return new SearchResultsPage();
     }
 
     public SearchResultsPage filter(List<ProductFilter> types) {
-        types.forEach(filter -> $x(String.format("//a[@data-id = '%s']", filter.getFilterXpath())).click());
+        types.forEach(filter -> $x(String.format("//a[@data-id = '%s']", filter.getFilterXpath()))
+                .scrollIntoView(false)
+                .click());
         return new SearchResultsPage();
     }
 }
