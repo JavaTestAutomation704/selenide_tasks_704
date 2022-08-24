b108f42fe8e3e9e957c834344ee761c023eb4a80
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-08-24 13:59:32 +0300

    mvynokur: add test to verify availability filter

---
+++ src/test/java/com/softserveinc/ita/rozetka/FilterProductTest.java
@@ -0,0 +1,35 @@
+package com.softserveinc.ita.rozetka;
+
+import com.softserveinc.ita.rozetka.data.Category;
+import com.softserveinc.ita.rozetka.data.Subcategory;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.softserveinc.ita.rozetka.data.ProductFilter.AVAILABLE;
+
+public class FilterProductTest extends TestRunner{
+
+    @Test
+    public void verifyProductAvailabilityFilter() {
+        homePage
+                .openCategoryPage(Category.NOTEBOOKS_COMPUTERS)
+                .openSubcategoryPage(Subcategory.NOTEBOOKS)
+                .getFilter()
+                .filter(AVAILABLE);
+
+        List<String> productAvailability = new ArrayList<>();
+        for (int i = 1; i <= 20; i++) {
+            productAvailability.add(new SearchResultsPage().get(i).getAvailability());
+        }
+
+        SoftAssert softAssert = new SoftAssert();
+        productAvailability
+                .forEach(availability -> softAssert.assertTrue(availability.contains("Готовий до відправлення")
+                                || availability.contains("Є у наявності"),
+                        "Product is not available"));
+        softAssert.assertAll();
+    }
+}
