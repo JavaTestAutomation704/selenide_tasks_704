0f42813a7bb7502580ae52b76b98d75f8cc936bf
DankoMaksym <dankomaksym@gmail.com>
2022-08-25 19:38:05 +0300

    dankomax: created CharacteristicsPageTest

---
+++ src/test/java/com/softserveinc/ita/rozetka/CharacteristicsPageTest.java
@@ -0,0 +1,35 @@
+package com.softserveinc.ita.rozetka;
+
+import com.softserveinc.ita.rozetka.components.Product;
+import com.softserveinc.ita.rozetka.data.Category;
+import com.softserveinc.ita.rozetka.data.subcategory.page.HouseholdAppliances;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class CharacteristicsPageTest extends TestRunner {
+    @Test
+    public void verifyUserCanOpenCharacteristicsPageTest() {
+        Product product = homePage
+                .openCategoryPage(Category.HOUSEHOLD_APPLIANCES)
+                .openSubcategoryPage(HouseholdAppliances.REFRIGERATORS)
+                .getProduct("last");
+        String productTitle = product.getTitle();
+
+        ProductCharacteristicsPage characteristicsPage = product
+                .open()
+                .openCharacteristicsPage();
+        String url = characteristicsPage.getUrl();
+        String characteristicsPageTitle = characteristicsPage.getTitle();
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(characteristicsPageTitle.contains(productTitle),
+                String.format("\nCharacteristics page title: '%s' \nShould contain: '%s'\n", characteristicsPageTitle, productTitle));
+        softAssert.assertTrue(characteristicsPageTitle.contains("характеристики"),
+                String.format("\nCharacteristics page title: '%s' \nShould contain: 'характеристики'\n", characteristicsPageTitle));
+        softAssert.assertTrue(characteristicsPage.isCharacteristicsTabHighlighted(),
+                "\nProduct characteristics tab is not highlighted.\n");
+        softAssert.assertTrue(url.contains("characteristics"),
+                String.format("\nPage url: '%s'. \nShould contain: 'characteristics'\n", url));
+        softAssert.assertAll();
+    }
+}
