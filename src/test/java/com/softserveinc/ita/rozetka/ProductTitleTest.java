e5d5cd95ac9a2a84282b0f6159e5e34c90f0d31b
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-08-26 00:17:09 +0300

    nastiakomarenko: changed verifyThatProductTitleContainsTheSameTitleAsCard() method

--- src/test/java/com/softserveinc/ita/rozetka/ProductTitleTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/ProductTitleTest.java
@@ -1,33 +1,31 @@
 package com.softserveinc.ita.rozetka;
 
 import com.softserveinc.ita.rozetka.data.Category;
-import com.softserveinc.ita.rozetka.data.ProductSort;
-import com.softserveinc.ita.rozetka.data.Subcategory;
+import com.softserveinc.ita.rozetka.data.subcategory.page.HouseholdAppliances;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.testng.Assert.assertTrue;
+
 public class ProductTitleTest extends TestRunner {
 
     @Test
-    public void verifyProductTitleContainsTheSameTitleAsCard() {
-        //this method will be changed
-        SearchResultsPage searchResultsPage = new SearchResultsPage();
-        homePage
-                .openCategoryPage(Category.NOTEBOOKS_COMPUTERS)
-                .openSubcategoryPage(Subcategory.NOTEBOOKS);
+    public void verifyThatProductTitleContainsTheSameTitleAsCard() {
+        SubcategoryPage products = homePage
+                .getHeader()
+                .openCatalogModal()
+                .openSubcategory(Category.HOUSEHOLD_APPLIANCES, HouseholdAppliances.REFRIGERATORS);
 
-        List<String> productTitles = new ArrayList<>();
-        productTitles.add(searchResultsPage.get(1).getTitle());
-        productTitles.add(searchResultsPage.get(5).getTitle());
-        productTitles.add(searchResultsPage.get(10).getTitle());
-        productTitles.add(searchResultsPage.get(20).getTitle());
+        String productName = "Холодильник";
 
-        SoftAssert softAssert = new SoftAssert();
-        productTitles.forEach(productTitle -> softAssert.assertTrue(productTitle.contains("Ноутбук"),
-                "Product title doesn't contain 'Ноутбук'"));
-        softAssert.assertAll();
+        List<String> productNameList = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            productNameList.add(products.getProduct(i).getTitle());
+        }
+
+        productNameList.forEach(productTitle -> assertTrue(productTitle.contains(productName),
+                String.format("Product title doesn't contain '%s'", productName)));
     }
 }
