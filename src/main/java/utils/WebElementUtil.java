5f361b222b178811526ca3a3bd88edb8e5d82629
DankoMaksym <dankomaksym@gmail.com>
2022-08-25 11:43:11 +0300

    dankomax: moved simple waits from Util to methods

--- src/main/java/utils/WebElementUtil.java
+++ src/main/java/utils/WebElementUtil.java
@@ -1,11 +1,8 @@
 package utils;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import lombok.experimental.UtilityClass;
 
 import java.time.Duration;
-import java.util.List;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 import static com.codeborne.selenide.Condition.*;
@@ -18,7 +15,7 @@ public class WebElementUtil {
 
     public static boolean isVisible(String elementXpath) {
         try {
-            return waitVisibility(elementXpath).isDisplayed();
+            return $x(elementXpath).shouldBe(visible, TIMEOUT).isDisplayed();
         } catch (AssertionError e) {
             return false;
         }
@@ -40,28 +37,8 @@ public class WebElementUtil {
     }
 
     public static long getLong(String elementXpath) {
-        return Long.parseLong( waitVisibility(elementXpath)
+        return Long.parseLong($x(elementXpath).shouldBe(visible, TIMEOUT)
                 .text()
-                .replaceAll("[^0-9]",""));
-    }
-
-    public static SelenideElement waitVisibility(String elementXpath) {
-        return $x(elementXpath).shouldBe(visible, TIMEOUT);
-    }
-
-    public static SelenideElement waitVisibility(SelenideElement element) {
-        return element.shouldBe(visible, TIMEOUT);
-    }
-
-    public static SelenideElement waitInvisibility(String elementXpath) {
-        return $x(elementXpath).shouldNotBe(visible, TIMEOUT);
-    }
-
-    public static SelenideElement waitTextChange(String elementXpath, String text) {
-        return $x(elementXpath).shouldNotBe(Condition.text(text), TIMEOUT);
-    }
-
-    public static List<SelenideElement> waitCollection(String elementsXpath) {
-        return $$x(elementsXpath).shouldBe(sizeGreaterThanOrEqual(1));
+                .replaceAll("[^0-9]", ""));
     }
 }
