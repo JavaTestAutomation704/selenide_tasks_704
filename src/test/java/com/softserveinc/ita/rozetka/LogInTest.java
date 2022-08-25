b8621bee398483a3a18a559801c8c8bfcf4331ea
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-25 20:03:24 +0300

    ykireyeva: improved verifyLoggingCapability test

--- src/test/java/com/softserveinc/ita/rozetka/LogInTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/LogInTest.java
@@ -1,18 +1,45 @@
 package com.softserveinc.ita.rozetka;
 
+import com.softserveinc.ita.rozetka.modals.LogInModal;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertTrue;
+import org.testng.asserts.SoftAssert;
 
 public class LogInTest extends TestRunner {
 
     @Test
     public void verifyLoggingCapability() {
-        boolean isLogInModalOpen = homePage
+        final String redColor = "rgb(248, 65, 71)";
+        final String errorMessageIncorrectEmail = "Введено невірну адресу ел. пошти або номер телефону";
+
+        LogInModal logInModal = homePage
                 .getHeader()
-                .startLoggingIn()
+                .startLoggingIn();
+
+        boolean isLogInModalOpen = logInModal.isOpen();
+        boolean isLogInButtonVisible = logInModal.isLogInButtonVisible();
+        boolean isRegistrationButtonVisible = logInModal.isRegistrationButtonVisible();
+
+        String actualEmailErrorMessage = logInModal
+                .logIn()
+                .getEmailErrorMessage();
+
+        String actualEmailBorderColor = logInModal.getEmailBorderColor(redColor);
+        String actualPasswordBorderColor = logInModal.getPasswordBorderColor(redColor);
+
+        boolean isRegistrationAvailable = logInModal
+                .startRegistration()
                 .isOpen();
 
-        assertTrue(isLogInModalOpen);
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertTrue(isLogInModalOpen);
+        softAssert.assertTrue(isLogInButtonVisible);
+        softAssert.assertTrue(isRegistrationButtonVisible);
+        softAssert.assertEquals(actualEmailErrorMessage, errorMessageIncorrectEmail);
+        softAssert.assertEquals(actualEmailBorderColor, redColor);
+        softAssert.assertEquals(actualPasswordBorderColor, redColor);
+        softAssert.assertTrue(isRegistrationAvailable);
+
+        softAssert.assertAll();
     }
 }
