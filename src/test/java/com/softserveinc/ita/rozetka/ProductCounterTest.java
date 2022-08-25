94355bf7a7245ac105f573172b42fee54938e680
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-08-25 21:37:14 +0300

    ozakharchuk: added test to verify product counter in shopping cart

---
+++ src/test/java/com/softserveinc/ita/rozetka/ProductCounterTest.java
@@ -0,0 +1,11 @@
+package com.softserveinc.ita.rozetka;
+
+import org.testng.annotations.Test;
+
+public class ProductCounterTest extends TestRunner {
+
+    @Test
+    public void verifyProductCounterWorks(){
+
+    }
+}
