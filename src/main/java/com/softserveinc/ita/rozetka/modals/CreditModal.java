d5bd6b2f777a684c802c66fb32202d3efdb1e0b9
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-25 20:32:05 +0300

    ykireyeva: added new functionality to CreditModal

--- src/main/java/com/softserveinc/ita/rozetka/modals/CreditModal.java
+++ src/main/java/com/softserveinc/ita/rozetka/modals/CreditModal.java
@@ -1,5 +1,8 @@
 package com.softserveinc.ita.rozetka.modals;
 
+import com.softserveinc.ita.rozetka.CheckoutPage;
+import com.softserveinc.ita.rozetka.CreditPage;
+
 import static com.codeborne.selenide.Selenide.$x;
 import static utils.WebElementUtil.isVisible;
 
@@ -12,5 +15,15 @@ public class CreditModal {
 
     public boolean isOpen() {
         return isVisible("//credit-modal");
+    }
+
+    public CreditPage readDetailedInfo() {
+        $x("//a[contains(@class, 'caption-link')]").click();
+        return new CreditPage();
+    }
+
+    public CheckoutPage chooseCreditVariant(int number) {
+        $x(String.format("(//rz-credit-variant//div//button)[%d]", number)).click();
+        return new CheckoutPage();
     }
 }
