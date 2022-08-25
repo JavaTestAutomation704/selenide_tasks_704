5e94139bcf9e3bc16139208d82adbee643247c0c
DankoMaksym <dankomaksym@gmail.com>
2022-08-25 19:33:24 +0300

    dankomax: solved issues with waits in PO

--- src/main/java/com/softserveinc/ita/rozetka/components/Header.java
+++ src/main/java/com/softserveinc/ita/rozetka/components/Header.java
@@ -4,11 +4,10 @@ import com.softserveinc.ita.rozetka.HomePage;
 import com.softserveinc.ita.rozetka.modals.LogInModal;
 import com.softserveinc.ita.rozetka.SearchResultsPage;
 import com.softserveinc.ita.rozetka.modals.ShoppingCartModal;
+import com.softserveinc.ita.rozetka.modals.CatalogModal;
 import org.openqa.selenium.By;
 
-import com.softserveinc.ita.rozetka.HomePage;
-import com.softserveinc.ita.rozetka.modals.CatalogModal;
-
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 import static utils.WebElementUtil.isVisible;
@@ -31,23 +30,28 @@ public class Header {
 
     public ShoppingCartModal openShoppingCartModal() {
         $x("//button[@rzopencart='']").click();
+        $x("//div[contains(@class, 'modal__holder')]").shouldBe(visible);
         return new ShoppingCartModal();
     }
 
     public boolean isShoppingCartCounterVisible() {
-        return isVisible("//button[@rzopencart='']//span[contains(@class, 'counter')]");
+        return isVisible("//button[@rzopencart='']//span[contains(@class, 'counter')]", 3);
     }
-    
+
     public HomePage openHomePageViaLogo() {
         $x(("//a[@class='header__logo']")).click();
         return new HomePage();
     }
+
     public boolean isRegisterButtonVisible() {
         return isVisible
                 ("//button[@class='auth-modal__register-link button button--link ng-star-inserted']");
     }
-    public CatalogModal openCatalogModal(){
-        $("button button--medium button--with-icon menu__toggle ng-star-inserted").click();
+
+    public CatalogModal openCatalogModal() {
+        $x("//button[@id='fat-menu']").click();
+        $x("//a[contains(@href, 'computers-notebooks')]/ancestor::li[contains(@class, 'categories__item')]//div[contains(@class, 'content')]")
+                .shouldBe(visible);
         return new CatalogModal();
     }
 }
