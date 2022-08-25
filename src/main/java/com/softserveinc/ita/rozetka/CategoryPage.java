397e38f01a5e94c0da795f3eb00343bcedbae281
DankoMaksym <dankomaksym@gmail.com>
2022-08-25 16:13:16 +0300

    dankomax: added subcategory interface with enums

--- src/main/java/com/softserveinc/ita/rozetka/CategoryPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/CategoryPage.java
@@ -1,12 +1,12 @@
 package com.softserveinc.ita.rozetka;
 
-import com.softserveinc.ita.rozetka.data.Subcategory;
+import com.softserveinc.ita.rozetka.data.subcategory.ISubcategory;
 
 import static com.codeborne.selenide.Selenide.$x;
 
 public class CategoryPage extends BasePage {
 
-    public SubcategoryPage openSubcategoryPage(Subcategory subcategory) {
+    public SubcategoryPage openSubcategoryPage(ISubcategory subcategory) {
         $x(String.format("//a[contains(@class, 'tile-cats__heading') and contains(@href, '%s')]",
                 subcategory.getSubcategoryXpath())).click();
         return new SubcategoryPage();
---
+++ src/main/java/com/softserveinc/ita/rozetka/data/subcategory/page/HouseholdAppliances.java
@@ -0,0 +1,21 @@
+package com.softserveinc.ita.rozetka.data.subcategory.page;
+
+import com.softserveinc.ita.rozetka.data.subcategory.ISubcategory;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum HouseholdAppliances implements ISubcategory {
+
+    REFRIGERATORS("refrigerators");
+
+    @NonNull
+    private final String subcategoryXpath;
+
+    @Override
+    public String getSubcategoryXpath() {
+        return subcategoryXpath;
+    }
+}
