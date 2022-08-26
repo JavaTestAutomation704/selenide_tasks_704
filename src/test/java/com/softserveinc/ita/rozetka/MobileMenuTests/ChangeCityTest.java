2635f2e65b90b9bd31a1866fb47eeb5ce6b04680
Olenka <olenkadashinich@gmail.com>
2022-08-26 18:44:34 +0300

    odashynych: added test to cover removing products from shopping cart

--- src/test/java/com/softserveinc/ita/rozetka/MobileMenuTests/ChangeCityTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/MobileMenuTests/ChangeCityTest.java
@@ -2,32 +2,35 @@ package com.softserveinc.ita.rozetka.MobileMenuTests;
 
 import com.softserveinc.ita.rozetka.TestRunner;
 import com.softserveinc.ita.rozetka.components.Header;
+import com.softserveinc.ita.rozetka.components.MobileMenu;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class ChangeCityTest extends TestRunner {
     @Test()
-    public void verifyChangeCityTest() {
+    public void verifyChangeCity() {
         String expectedCityViaMobileMenu = "Одеса";
-        Header header = homePage
-                .getHeader()
+        Header header = homePage.getHeader();
+        MobileMenu mobileMenu = header
                 .openMobileMenu()
-                .changeCity(expectedCityViaMobileMenu);
+                .changeCity(expectedCityViaMobileMenu)
+                .openMobileMenu();
 
-        String actualCityViaMobileMenu = header
-                .openMobileMenu()
+        String actualCityViaMobileMenu = mobileMenu
                 .getCity();
 
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(actualCityViaMobileMenu, expectedCityViaMobileMenu);
 
         String expectedCityViaProductPage = "Дніпро";
-        String actualCityViaProductPage = header
+        header = header
                 .search("Планшети")
                 .openProductPage(1)
                 .changeCity(expectedCityViaProductPage)
-                .getHeader()
-                .openMobileMenu()
+                .getHeader();
+        mobileMenu = header.openMobileMenu();
+
+        String actualCityViaProductPage = mobileMenu
                 .getCity();
 
         softAssert.assertEquals(actualCityViaProductPage, expectedCityViaProductPage);
