b979a7510914eec7ab62078722a2692b9082bd2e
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-26 09:36:04 +0300

    ykireyeva: refactored verifyLoggingCapability test

--- src/test/java/com/softserveinc/ita/rozetka/LogInTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/LogInTest.java
@@ -4,40 +4,43 @@ import com.softserveinc.ita.rozetka.modals.LogInModal;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import static org.testng.Assert.assertTrue;
+
 public class LogInTest extends TestRunner {
 
     @Test
     public void verifyLoggingCapability() {
-        final String redColor = "rgb(248, 65, 71)";
-        final String errorMessageIncorrectEmail = "Введено невірну адресу ел. пошти або номер телефону";
 
         LogInModal logInModal = homePage
                 .getHeader()
                 .startLoggingIn();
 
-        boolean isLogInModalOpen = logInModal.isOpen();
-        boolean isLogInButtonVisible = logInModal.isLogInButtonVisible();
-        boolean isRegistrationButtonVisible = logInModal.isRegistrationButtonVisible();
+        assertTrue(logInModal.isOpen());
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertTrue(logInModal.isLogInButtonVisible());
+        softAssert.assertTrue(logInModal.isRegistrationButtonVisible());
 
         String actualEmailErrorMessage = logInModal
                 .logIn()
                 .getEmailErrorMessage();
 
+        softAssert.assertEquals(actualEmailErrorMessage,
+                "Введено невірну адресу ел. пошти або номер телефону");
+
+        final String redColor = "rgb(248, 65, 71)";
+
         String actualEmailBorderColor = logInModal.getEmailBorderColor(redColor);
         String actualPasswordBorderColor = logInModal.getPasswordBorderColor(redColor);
+
+        softAssert.assertEquals(actualEmailBorderColor, redColor);
+        softAssert.assertEquals(actualPasswordBorderColor, redColor);
 
         boolean isRegistrationAvailable = logInModal
                 .startRegistration()
                 .isOpen();
 
-        SoftAssert softAssert = new SoftAssert();
-
-        softAssert.assertTrue(isLogInModalOpen);
-        softAssert.assertTrue(isLogInButtonVisible);
-        softAssert.assertTrue(isRegistrationButtonVisible);
-        softAssert.assertEquals(actualEmailErrorMessage, errorMessageIncorrectEmail);
-        softAssert.assertEquals(actualEmailBorderColor, redColor);
-        softAssert.assertEquals(actualPasswordBorderColor, redColor);
         softAssert.assertTrue(isRegistrationAvailable);
 
         softAssert.assertAll();
