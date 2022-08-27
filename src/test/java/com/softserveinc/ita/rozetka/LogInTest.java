02099c6d7efe8c646ac60b110d081a85b6888de0
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-28 00:19:14 +0300

    ykireyeva: added AssertJ assertions to verifyLoggingCapability test

--- src/test/java/com/softserveinc/ita/rozetka/LogInTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/LogInTest.java
@@ -1,10 +1,10 @@
 package com.softserveinc.ita.rozetka;
 
 import com.softserveinc.ita.rozetka.modals.LogInModal;
+import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
-import static org.testng.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LogInTest extends TestRunner {
 
@@ -15,33 +15,54 @@ public class LogInTest extends TestRunner {
                 .getHeader()
                 .startLoggingIn();
 
-        assertTrue(logInModal.isOpen());
+        assertThat(logInModal.isOpen())
+                .withFailMessage("Log In modal did not open")
+                .isTrue();
 
-        SoftAssert softAssert = new SoftAssert();
+        SoftAssertions softAssert = new SoftAssertions();
 
-        softAssert.assertTrue(logInModal.isLogInButtonVisible());
-        softAssert.assertTrue(logInModal.isRegistrationButtonVisible());
+        softAssert
+                .assertThat(logInModal.isLogInButtonVisible())
+                .withFailMessage("Log In button did not display")
+                .isTrue();
+
+        softAssert
+                .assertThat(logInModal.isRegistrationButtonVisible())
+                .withFailMessage("Registration button did not display")
+                .isTrue();
 
         String actualEmailErrorMessage = logInModal
                 .logIn()
                 .getEmailErrorMessage();
 
-        softAssert.assertEquals(actualEmailErrorMessage,
-                "Введено невірну адресу ел. пошти або номер телефону");
+        softAssert
+                .assertThat(actualEmailErrorMessage)
+                .withFailMessage("Expected error message did not appear")
+                .isEqualTo("Введено невірну адресу ел. пошти або номер телефону");
 
         final String redColor = "rgb(248, 65, 71)";
 
         String actualEmailBorderColor = logInModal.getEmailBorderColor(redColor);
         String actualPasswordBorderColor = logInModal.getPasswordBorderColor(redColor);
 
-        softAssert.assertEquals(actualEmailBorderColor, redColor);
-        softAssert.assertEquals(actualPasswordBorderColor, redColor);
+        softAssert
+                .assertThat(actualEmailBorderColor)
+                .withFailMessage("Incorrect border color")
+                .isEqualTo(redColor);
+
+        softAssert
+                .assertThat(actualPasswordBorderColor)
+                .withFailMessage("Incorrect border color")
+                .isEqualTo(redColor);
 
         boolean isRegistrationAvailable = logInModal
                 .startRegistration()
                 .isOpen();
 
-        softAssert.assertTrue(isRegistrationAvailable);
+        softAssert
+                .assertThat(isRegistrationAvailable)
+                .withFailMessage("Registration is unavailable via Log In modal")
+                .isTrue();
 
         softAssert.assertAll();
     }
