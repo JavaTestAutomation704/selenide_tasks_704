27642fc9d12d1880c6c4a1f1c3bee2305024ed54
DankoMaksym <dankomaksym@gmail.com>
2022-08-28 01:56:11 +0300

    dankomax: added action check on product Characteristics page

--- src/test/java/com/softserveinc/ita/rozetka/CharacteristicsPageTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/CharacteristicsPageTest.java
@@ -3,12 +3,12 @@ package com.softserveinc.ita.rozetka;
 import com.softserveinc.ita.rozetka.components.Product;
 import com.softserveinc.ita.rozetka.data.Category;
 import com.softserveinc.ita.rozetka.data.subcategory.page.HouseholdAppliances;
+import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 public class CharacteristicsPageTest extends TestRunner {
     @Test
-    public void verifyUserCanOpenCharacteristicsPageTest() {
+    public void verifyUserCanAddProductToComparisonOnCharacteristicsPage() {
         Product product = homePage
                 .openCategoryPage(Category.HOUSEHOLD_APPLIANCES)
                 .openSubcategoryPage(HouseholdAppliances.REFRIGERATORS)
@@ -21,15 +21,25 @@ public class CharacteristicsPageTest extends TestRunner {
         String url = characteristicsPage.getUrl();
         String characteristicsPageTitle = characteristicsPage.getTitle();
 
-        SoftAssert softAssert = new SoftAssert();
-        softAssert.assertTrue(characteristicsPageTitle.contains(productTitle),
-                String.format("\nCharacteristics page title: '%s' \nShould contain: '%s'\n", characteristicsPageTitle, productTitle));
-        softAssert.assertTrue(characteristicsPageTitle.contains("характеристики"),
-                String.format("\nCharacteristics page title: '%s' \nShould contain: 'характеристики'\n", characteristicsPageTitle));
-        softAssert.assertTrue(characteristicsPage.isCharacteristicsTabHighlighted(),
-                "\nProduct characteristics tab is not highlighted.\n");
-        softAssert.assertTrue(url.contains("characteristics"),
-                String.format("\nPage url: '%s'. \nShould contain: 'characteristics'\n", url));
-        softAssert.assertAll();
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(characteristicsPageTitle)
+                .as("Characteristics page title")
+                .contains(productTitle);
+        softly.assertThat(characteristicsPageTitle)
+                .as("Characteristics page title")
+                .contains("характеристики");
+        softly.assertThat(characteristicsPage.isCharacteristicsTabHighlighted())
+                .as("Product characteristics tab should be highlighted")
+                .isTrue();
+        softly.assertThat(url)
+                .as("Page url")
+                .contains("characteristics");
+
+        characteristicsPage.addToComparison();
+
+        softly.assertThat(characteristicsPage.isComparisonCounterVisible())
+                .as("Comparison counter is visible")
+                .isTrue();
+        softly.assertAll();
     }
 }
