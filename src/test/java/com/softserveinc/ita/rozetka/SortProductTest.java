3e2f19b8a53fd44b7b6b164ae69938c3d6ba9d02
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-08-28 23:10:59 +0300

    nastiakomarenko: changed assert to assertj in verifyUserCanSortProductsInAscendingOrderByPriceTest() method

--- src/test/java/com/softserveinc/ita/rozetka/SortProductTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/SortProductTest.java
@@ -3,12 +3,11 @@ package com.softserveinc.ita.rozetka;
 import com.softserveinc.ita.rozetka.data.Category;
 import com.softserveinc.ita.rozetka.data.ProductSort;
 import com.softserveinc.ita.rozetka.data.subcategory.page.HouseholdAppliances;
+import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.testng.Assert.*;
 
 public class SortProductTest extends TestRunner {
     @Test
@@ -20,12 +19,14 @@ public class SortProductTest extends TestRunner {
         products.sortBy(ProductSort.PRICE_ASCENDING);
 
         List<Long> productPrice = new ArrayList<>(products.getProductsSize());
+        SoftAssertions softAssert = new SoftAssertions();
 
         for (int i = 1; i < products.getProductsSize(); i++) {
             productPrice.add(products.getProduct(i).getPrice());
             if ((i % 3 == 0) && (i > 2)) {
-                assertTrue(productPrice.get(i - 1) >= productPrice.get(i - 2),
-                        String.format("%d product price should be higher than %d",
+                softAssert
+                        .assertThat(productPrice.get(i - 1) >= productPrice.get(i - 2))
+                        .withFailMessage(String.format("%d product price should be higher than %d",
                                 productPrice.get(i - 1), productPrice.get(i - 2)));
             }
         }
