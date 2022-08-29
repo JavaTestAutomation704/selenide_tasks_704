ac0c42b461bdd12e86a66cb83a7de88c68a39b2e
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-08-29 19:43:11 +0300

    nastiakomarenko: added assertThat in verifyUserCanSortProductsInAscendingOrderByPrice() method

--- src/test/java/com/softserveinc/ita/rozetka/SortProductTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/SortProductTest.java
@@ -2,27 +2,30 @@ package com.softserveinc.ita.rozetka;
 
 import com.softserveinc.ita.rozetka.data.Category;
 import com.softserveinc.ita.rozetka.data.ProductSort;
-import com.softserveinc.ita.rozetka.data.subcategory.page.HouseholdAppliances;
+import com.softserveinc.ita.rozetka.data.subcategory.modal.LaptopsAndComputers;
 import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.Test;
 
 public class SortProductTest extends TestRunner {
     @Test
     public void verifyUserCanSortProductsInAscendingOrderByPrice() {
-        SearchResultsPage searchResultsPage = homePage
-                .openCategoryPage(Category.HOUSEHOLD_APPLIANCES)
-                .openSubcategoryPage(HouseholdAppliances.REFRIGERATORS);
+        SubcategoryPage subcategoryPage = homePage
+                .getHeader()
+                .openCatalogModal()
+                .openSubcategory(Category.LAPTOPS_AND_COMPUTERS, LaptopsAndComputers.ASUS);
 
-        searchResultsPage.sortBy(ProductSort.PRICE_ASCENDING);
+        subcategoryPage.sortBy(ProductSort.PRICE_ASCENDING);
         SoftAssertions softAssert = new SoftAssertions();
 
-        if (searchResultsPage.getProductsSize() >= 1) {
-            for (int i = 2; i < searchResultsPage.getProductsSize(); i++) {
-                softAssert
-                        .assertThat(searchResultsPage.getProduct(i).getPrice() >= searchResultsPage.getProduct(i-1).getPrice())
-                        .withFailMessage(String.format("%d product price should be higher than %d",
-                                searchResultsPage.getProduct(i).getPrice(), searchResultsPage.getProduct(i).getPrice()));
-            }
+        softAssert.assertThat(subcategoryPage.getProductsQuantity())
+                .as("Products quantity should be sufficient")
+                .isGreaterThanOrEqualTo(2);
+
+        for (int i = 2; i < subcategoryPage.getProductsQuantity(); i++) {
+            softAssert
+                    .assertThat(subcategoryPage.getProduct(i).getPrice() >= subcategoryPage.getProduct(i - 1).getPrice())
+                    .as(String.format("%d product price should be higher than %d",
+                            subcategoryPage.getProduct(i).getPrice(), subcategoryPage.getProduct(i).getPrice()));
         }
     }
 }
