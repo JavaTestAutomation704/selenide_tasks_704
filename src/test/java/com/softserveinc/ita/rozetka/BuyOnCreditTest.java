2abb820bfb658ae9014634713ec32d4e4b336e7c
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-08-30 23:52:30 +0300

    ykireyeva: corrected BuyOnCreditTest

--- src/test/java/com/softserveinc/ita/rozetka/BuyOnCreditTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/BuyOnCreditTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import static com.softserveinc.ita.rozetka.data.ProductFilter.ROZETKA_SELLER;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.assertTrue;
 
 public class BuyOnCreditTest extends TestRunner {
@@ -13,12 +14,20 @@ public class BuyOnCreditTest extends TestRunner {
     @Test
     public void verifyPurchaseOnCreditCapability() {
 
-        ProductPage productPage = homePage
+        SearchResultsPage searchResultsPage = homePage
                 .getHeader()
                 .search("Xbox")
                 .getFilter()
-                .filter(ROZETKA_SELLER)
-                .getProduct(1)
+                .filter(ROZETKA_SELLER);
+
+        int productNumber = 1;
+
+        assertThat(searchResultsPage.getProductsQuantity())
+                .as(String.format("There should be at least %d product on the search results page", productNumber))
+                .isGreaterThanOrEqualTo(productNumber);
+
+        ProductPage productPage = searchResultsPage
+                .getProduct(productNumber)
                 .open();
 
         CreditModal creditModal = productPage.startPurchaseOnCredit();
