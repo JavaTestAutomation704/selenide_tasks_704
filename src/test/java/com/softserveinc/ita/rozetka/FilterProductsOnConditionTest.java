204882321df199f37e2389883c42b40947b5a578
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-08-30 13:36:31 +0300

    ozakharchuk: edited test to verify filter products

--- src/test/java/com/softserveinc/ita/rozetka/FilterProductsOnConditionTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/FilterProductsOnConditionTest.java
@@ -3,7 +3,8 @@ package com.softserveinc.ita.rozetka;
 import com.softserveinc.ita.rozetka.components.Filter;
 import com.softserveinc.ita.rozetka.data.Category;
 import com.softserveinc.ita.rozetka.data.ProductFilter;
-import com.softserveinc.ita.rozetka.data.subcategory.modal.LaptopsAndComputers;
+import com.softserveinc.ita.rozetka.data.subcategory.LaptopsAndComputersSubcategory;
+import com.softserveinc.ita.rozetka.utils.TestRunner;
 import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.Test;
 
@@ -13,17 +14,18 @@ public class FilterProductsOnConditionTest extends TestRunner {
     public void verifyProductConditionFilter() {
         SubcategoryPage subcategoryPage = homePage
                 .openCategoryPage(Category.LAPTOPS_AND_COMPUTERS)
-                .openSubcategoryPage(LaptopsAndComputers.NOTEBOOKS);
+                .openSubcategoryPage(LaptopsAndComputersSubcategory.NOTEBOOKS);
         Filter filter = subcategoryPage.getFilter();
         filter.filter(ProductFilter.PRE_USED);
         int productsQuantity = subcategoryPage.getProductsQuantity();
-        int productsQuantityToCheck = 10;
+        int productsQuantityToCheck = 30;
 
         SoftAssertions softly = new SoftAssertions();
 
         softly.assertThat(productsQuantity)
                 .as("Products quantity should be sufficient")
                 .isGreaterThanOrEqualTo(productsQuantityToCheck);
+
         for (int i = 1; i <= productsQuantity; i = i + 10) {
             softly.assertThat(subcategoryPage.getProduct(i).isUsed())
                     .as("Product should be used")
@@ -36,6 +38,7 @@ public class FilterProductsOnConditionTest extends TestRunner {
         softly.assertThat(productsQuantity)
                 .as("Products quantity should be sufficient")
                 .isGreaterThanOrEqualTo(productsQuantityToCheck);
+
         for (int i = 1; i <= subcategoryPage.getProductsQuantity(); i = i + 10) {
             softly.assertThat(subcategoryPage.getProduct(i).isUsed())
                     .as("Product should be new")
