1116920ee67aab8564c6c8c7e4ea69ac2c73a911
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-08-31 17:54:47 +0300

    ozakharchuk: moved ProductCounterTest

--- src/test/java/com/softserveinc/ita/rozetka/ShoppingCartTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/ShoppingCartTest.java
@@ -1,5 +1,6 @@
 package com.softserveinc.ita.rozetka;
 
+import com.softserveinc.ita.rozetka.components.CartItem;
 import com.softserveinc.ita.rozetka.components.Header;
 import com.softserveinc.ita.rozetka.components.Product;
 import com.softserveinc.ita.rozetka.modals.ShoppingCartModal;
@@ -130,5 +131,56 @@ public class ShoppingCartTest extends TestRunner {
         assertThat(actualTotalSum)
                 .as("Total sum in shopping cart should be equal to products prices sum")
                 .isEqualTo(expectedTotalSum);
+    }
+
+    @Test
+    public void verifyProductCounterWorks() {
+        Header header = homePage.getHeader();
+        boolean doesShoppingCartContainProducts = header.isShoppingCartCounterVisible();
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(doesShoppingCartContainProducts)
+                .as("Shopping cart should be empty")
+                .isFalse();
+        SearchResultsPage searchResultsPage = header.search("kipling");
+
+        softly.assertThat(searchResultsPage.getProductsQuantity())
+                .as("Products quantity should be sufficient")
+                .isGreaterThanOrEqualTo(1);
+
+        Product product = searchResultsPage.getProduct(1);
+        product.addToShoppingCart();
+
+        softly.assertThat(product.isInShoppingCart())
+                .as("First product should be added to shopping cart")
+                .isTrue();
+
+        long price = product.getPrice();
+
+        CartItem cartItem = header
+                .openShoppingCartModal()
+                .getItem(1);
+
+        int initialQuantity = cartItem.getQuantity();
+        cartItem.increment();
+        int increasedQuantity = cartItem.getQuantity();
+
+        softly.assertThat(increasedQuantity)
+                .as("Quantity should be increased")
+                .isEqualTo(initialQuantity + 1);
+        softly.assertThat(price + price)
+                .as(" Total price should be increased")
+                .isEqualTo(cartItem.getTotalPrice());
+
+        cartItem.decrement();
+        int decreasedQuantity = cartItem.getQuantity();
+
+        softly.assertThat(decreasedQuantity)
+                .as("Quantity should be decreased")
+                .isEqualTo(increasedQuantity - 1);
+        softly.assertThat(price)
+                .as(" Total price should be decreased")
+                .isEqualTo(cartItem.getTotalPrice());
+        softly.assertAll();
     }
 }
