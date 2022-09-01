9f27b6e9265797a38d317f7f0b2482d606ad3505
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-09-01 19:04:33 +0300

    ykireyeva: renamed verifyCorrectnessOfPromotionTerms test -> verifyPromotionTermsCorrespondence

--- src/test/java/com/softserveinc/ita/rozetka/PromotionsTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/PromotionsTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PromotionsTest extends TestRunner {
 
     @Test
-    public void verifyCorrectnessOfPromotionTerms() {
+    public void verifyPromotionTermsCorrespondence() {
 
         var promotionsPage = homePage.openPromotionsPage();
 
