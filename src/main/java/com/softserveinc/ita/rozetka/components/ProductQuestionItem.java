113ce982a0a31d89df349c46b2db77de7cf0005a
Nastia Komarenko <nastiakomarenko@gmail.com>
2022-09-13 01:28:55 +0300

    nastiakomarenko: added verifySortByDateProductQuestionsFunctionality() method for ProductQuestionsTest to cover product questions functionality

--- src/main/java/com/softserveinc/ita/rozetka/components/ProductQuestionItem.java
+++ src/main/java/com/softserveinc/ita/rozetka/components/ProductQuestionItem.java
@@ -1,5 +1,10 @@
 package com.softserveinc.ita.rozetka.components;
 
+import com.softserveinc.ita.rozetka.data.Month;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import static com.codeborne.selenide.Selenide.$x;
 import static com.softserveinc.ita.rozetka.utils.WebElementUtil.isVisible;
 
@@ -13,5 +18,17 @@ public class ProductQuestionItem {
 
     public boolean hasPhoto() {
         return isVisible(productQuestionItemXpath + "//ul[@class='product-comments__photos-list ng-star-inserted']", 2);
+    }
+
+    public Date getDate() {
+        return parseDate($x(productQuestionItemXpath + "//time").text());
+    }
+
+    private Date parseDate(String inputDate) {
+        var dateParts = inputDate.split(" ");
+        var calendar = new GregorianCalendar(Integer.parseInt(dateParts[2]), Month.getNumberByValue(dateParts[1]),
+                Integer.parseInt(dateParts[0]));
+
+        return calendar.getTime();
     }
 }
