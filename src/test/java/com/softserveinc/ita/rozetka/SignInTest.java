a4a34a968b12b578bcf7a9363929a56b779ab9bf
Jaroslav Tarasovych <staryk90@gmail.com>
2022-09-15 22:55:42 +0300

    ytarasovych: edit login test to verify

--- src/test/java/com/softserveinc/ita/rozetka/SignInTest.java
+++ src/test/java/com/softserveinc/ita/rozetka/SignInTest.java
@@ -1,5 +1,7 @@
 package com.softserveinc.ita.rozetka;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.softserveinc.ita.rozetka.utils.ConfigProperties;
 import com.softserveinc.ita.rozetka.utils.LoginTestRunner;
 import org.assertj.core.api.SoftAssertions;
@@ -9,19 +11,19 @@ public class SignInTest extends LoginTestRunner {
     private final ConfigProperties property = new ConfigProperties();
 
     @Test
-    public void verifySignInFunctionality() {
-        var personalCabinetPage = homePage
+    public void verifySignInFunctionalityWorks() {
+
+        var profilePage = homePage
                 .getHeader()
                 .openMyOrdersPage()
                 .openProfilePage();
-
         var softly = new SoftAssertions();
 
-        softly.assertThat(personalCabinetPage.getUserEmail())
+        softly.assertThat(profilePage.getUserEmail())
                 .as("User name should be correct")
                 .isEqualTo(property.getUserEmail());
 
-        var isSignInModalVisible = personalCabinetPage
+        var isSignInModalVisible = profilePage
                 .signOut()
                 .getHeader()
                 .openSignInModal()
