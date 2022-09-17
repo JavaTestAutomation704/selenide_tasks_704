f8598ddcecdbc503625348b38242294d6546644f
DankoMaksym <dankomaksym@gmail.com>
2022-09-17 12:55:05 +0300

    dankomax: solved DrinkingAgeConfirmationModal confirmation issue

--- src/main/java/com/softserveinc/ita/rozetka/modals/DrinkingAgeConfirmationModal.java
+++ src/main/java/com/softserveinc/ita/rozetka/modals/DrinkingAgeConfirmationModal.java
@@ -4,7 +4,6 @@ import com.softserveinc.ita.rozetka.SearchResultsPage;
 import com.softserveinc.ita.rozetka.SubcategoryPage;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.ClickOptions.usingJavaScript;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.softserveinc.ita.rozetka.utils.WebElementUtil.isVisible;
 
@@ -12,8 +11,14 @@ public class DrinkingAgeConfirmationModal {
     @Step("Drinking age confirmation modal: confirm user can legally consume alcoholic beverages")
     public SearchResultsPage confirm() {
         var confirmationButtonXpath = "//a[contains(@class, 'exponea-close')]";
-        if(isVisible(confirmationButtonXpath, 3)) {
-            $x(confirmationButtonXpath).click(usingJavaScript());
+        var confirmationButton = $x(confirmationButtonXpath);
+        var denyButton = $x("//div[contains(@class, 'banner')]//a[contains(@href, 'kids')]");
+
+        int counter = 0;
+        while(isVisible(confirmationButtonXpath, 3) && counter < 10) {
+            denyButton.hover();
+            confirmationButton.click();
+            counter++;
         }
         return new SubcategoryPage();
     }
