33f7432c2f16bd93d47d2ae2b26468b13895a78a
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-09-20 01:18:54 +0300

    ykireyeva: added openProfilePage() to ProfileSidebar

--- src/main/java/com/softserveinc/ita/rozetka/components/profile/ProfileSidebar.java
+++ src/main/java/com/softserveinc/ita/rozetka/components/profile/ProfileSidebar.java
@@ -1,5 +1,15 @@
 package com.softserveinc.ita.rozetka.components.profile;
 
+import com.softserveinc.ita.rozetka.profile.ProfilePage;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.$x;
+
 public class ProfileSidebar {
-    // created to navigate through pages for further testing
+
+    @Step("Profile sidebar: open profile page")
+    public ProfilePage openProfilePage() {
+        $x("//div[contains(@class,'cabinet-user')]/a").click();
+        return new ProfilePage();
+    }
 }
