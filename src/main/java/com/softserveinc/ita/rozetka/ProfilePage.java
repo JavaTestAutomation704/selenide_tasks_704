ee31ff1dfbaa7045a95554d9e6b445e840cad795
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-09-19 22:06:53 +0300

    ykireyeva: added openSection() method to ProfilePage

--- src/main/java/com/softserveinc/ita/rozetka/ProfilePage.java
+++ src/main/java/com/softserveinc/ita/rozetka/ProfilePage.java
@@ -1,15 +1,25 @@
 package com.softserveinc.ita.rozetka;
 
 import com.softserveinc.ita.rozetka.components.CabinetSidebar;
+import com.softserveinc.ita.rozetka.components.profile.sections.PersonalDataSection;
+import com.softserveinc.ita.rozetka.data.ProfileSection;
 import io.qameta.allure.Step;
 import lombok.Getter;
 
 import static com.codeborne.selenide.Selenide.$x;
+import static java.lang.String.format;
 
 @Getter
 public class ProfilePage {
 
     private final CabinetSidebar cabinetSidebar = new CabinetSidebar();
+    private final PersonalDataSection personalDataSection = new PersonalDataSection();
+
+    @Step("Profile page: open section {profileSection}")
+    public ProfilePage openSection(ProfileSection profileSection) {
+        $x(format("//h3[text()='%s']/ancestor::details", profileSection.getName())).click();
+        return this;
+    }
 
     @Step("Profile page: sign out")
     public HomePage signOut() {
---
+++ src/main/java/com/softserveinc/ita/rozetka/components/profile/sections/PersonalDataSection.java
@@ -0,0 +1,5 @@
+package com.softserveinc.ita.rozetka.components.profile.sections;
+
+public class PersonalDataSection {
+    // created for further testing
+}
