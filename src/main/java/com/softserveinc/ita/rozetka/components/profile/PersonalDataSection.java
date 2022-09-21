38113b05b4a1f0acef60ff36725ecd83f00342b7
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-09-21 12:50:28 +0300

    ykireyeva: added lombok annotation to PersonalDataSection

--- src/main/java/com/softserveinc/ita/rozetka/components/profile/PersonalDataSection.java
+++ src/main/java/com/softserveinc/ita/rozetka/components/profile/PersonalDataSection.java
@@ -1,38 +1,35 @@
 package com.softserveinc.ita.rozetka.components.profile;
 
+import com.softserveinc.ita.rozetka.models.PersonalData;
 import io.qameta.allure.Step;
+import lombok.RequiredArgsConstructor;
 
 import static com.codeborne.selenide.Selenide.$x;
 import static com.softserveinc.ita.rozetka.utils.WebElementUtil.*;
 
+@RequiredArgsConstructor
 public class PersonalDataSection {
 
     private final String personalDataSectionXpath;
-    private final String saveButtonXpath = "//button[@type='submit']";
     private final String inputFirstNameXpath = "//input[@id='firstName']";
     private final String inputSecondNameXpath = "//input[@id='secondName']";
     private final String inputLastNameXpath = "//input[@id='lastName']";
     private final String inputBirthdayXpath = "//input[@id='birthday']";
     private final String errorMessageXpath = "/ancestor::li//p";
 
-    public PersonalDataSection(String personalDataSectionXpath) {
-        this.personalDataSectionXpath = personalDataSectionXpath;
-    }
 
-
-    @Step("Personal data section: edit")
-    public PersonalDataSection edit() {
+    @Step("Personal data section: start editing")
+    public PersonalDataSection startEditing() {
         $x("//button[contains(@class,'personal-data__edit')]").click();
         return this;
     }
 
-    @Step("Personal data section: fill in personal data {lastName, firstName, secondName, birthday}")
-    public PersonalDataSection fillInPersonalData(String lastName, String firstName,
-                                                  String secondName, String birthday) {
-        $x(inputLastNameXpath).val(lastName);
-        $x(inputFirstNameXpath).val(firstName);
-        $x(inputSecondNameXpath).val(secondName);
-        $x(inputBirthdayXpath).val(birthday);
+    @Step("Personal data section: fill in personal data {personalData}")
+    public PersonalDataSection fillInPersonalData(PersonalData personalData) {
+        $x(inputLastNameXpath).val(personalData.getLastName());
+        $x(inputFirstNameXpath).val(personalData.getFirstName());
+        $x(inputSecondNameXpath).val(personalData.getSecondName());
+        $x(inputBirthdayXpath).val(personalData.getBirthday());
         return this;
     }
 
@@ -57,7 +54,7 @@ public class PersonalDataSection {
     }
 
     public boolean isSaveButtonDisabled() {
-        return hasAttribute(personalDataSectionXpath + saveButtonXpath, "disabled");
+        return hasAttribute(personalDataSectionXpath + "//button[@type='submit']", "disabled");
     }
 
     public boolean isFirstNameBorderColorCorrect(String colorRgb) {
