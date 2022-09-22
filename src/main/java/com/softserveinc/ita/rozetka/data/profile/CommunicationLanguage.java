8a1f69b52b2fc861a6c64a3728252dfae85d13cc
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-09-22 20:21:54 +0300

    ykireyeva: renamed enum Language -> CommunicationLanguage

---
+++ src/main/java/com/softserveinc/ita/rozetka/data/profile/CommunicationLanguage.java
@@ -0,0 +1,25 @@
+package com.softserveinc.ita.rozetka.data.profile;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommunicationLanguage {
+
+    UKRAINIAN("Українська"),
+    RUSSIAN("Русский");
+
+    @NonNull
+    private final String name;
+
+    public static CommunicationLanguage getByValue(String value) {
+        for (CommunicationLanguage communicationLanguage : values()) {
+            if (communicationLanguage.name.equals(value)) {
+                return communicationLanguage;
+            }
+        }
+        throw new IllegalArgumentException("Can't get CommunicationLanguage enum for value: " + value);
+    }
+}
