7afe423010dfc3094715d1a0b322944d70ca9ffc
DankoMaksym <dankomaksym@gmail.com>
2022-09-26 13:19:31 +0300

    dankomax: used lombok getter for defaultWishlist on WishlistPage

--- src/main/java/com/softserveinc/ita/rozetka/WishlistPage.java
+++ src/main/java/com/softserveinc/ita/rozetka/WishlistPage.java
@@ -3,16 +3,16 @@ package com.softserveinc.ita.rozetka;
 import com.softserveinc.ita.rozetka.components.Wishlist;
 import com.softserveinc.ita.rozetka.modals.AddWishlistModal;
 import io.qameta.allure.Step;
+import lombok.Getter;
 
 import java.util.List;
 
 import static com.codeborne.selenide.Selenide.$x;
 import static com.softserveinc.ita.rozetka.utils.WebElementUtil.*;
 
+@Getter
 public class WishlistPage extends BasePage {
-    public Wishlist getDefaultWishlist() {
-        return new Wishlist();
-    }
+    private final Wishlist defaultWishlist = new Wishlist();
 
     public Wishlist getWishlist(int wishlistNumber) {
         return new Wishlist(wishlistNumber);
